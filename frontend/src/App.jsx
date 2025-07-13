c41aec651939de51ad2cf6b763df63ca82e9cf41
Uchechukwu Alih <alihuchechukwumichael@gmail.com>
2025-07-13 04:25:00 -0500

    removed hardcoded url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 
-const API_URL = 'http://127.0.0.1:5000/api';
+const API_URL = '/api';
 
 function App() {
   const [posts, setPosts] = useState([]);
